ac995adbeb9b9b88392d5c0a0d88b001658e5e72
Daniel <danielomar510@gmail.com>
2026-05-19 10:35:19 -0600

    Primer commit - Estructura completa

---
+++ src/app/domain/noticias.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NoticiasService {
+  // Aquí el tipo guardará los métodos para jalar noticias más adelante
+  constructor() { }
+}
--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,19 +1,24 @@
 import { Component, OnInit } from '@angular/core'
 import { RadSideDrawer } from 'nativescript-ui-sidedrawer'
 import { Application } from '@nativescript/core'
+import { NoticiasService } from '../domain/noticias.service';
 
 @Component({
   selector: 'Search',
   templateUrl: './search.component.html',
 })
 export class SearchComponent implements OnInit {
-  constructor() {
+constructor(private noticias: NoticiasService) { 
     // Use the component constructor to inject providers.
-  }
+}
 
   ngOnInit(): void {
-    // Init your component properties here.
-  }
+    console.log("adasd");
+    console.log({nombre: {nombre: {nombre: {nombre: "pepe"}}}});
+    console.dir({nombre: {nombre: {nombre: {nombre: "pepe"}}}});
+    console.log([1, 2, 3]);
+    console.dir([4, 5, 6]);
+}
 
   onDrawerButtonTap(): void {
     const sideDrawer = <RadSideDrawer>Application.getRootView()
